56ded092553455aede69fe75af5f1d32115c18b5
vishalgaddam873 <vishal18@navgurukul.org>
2020-09-17 23:00:37 +0530

    UI changed

---
+++ components/MyHeader.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Header, Icon } from "react-native-elements";
+import { StyleSheet } from "react-native";
+
+const MyHeader = props => {
+  return (
+    <Header
+      leftComponent={
+        <Icon
+          name={"bars"}
+          type={"font-awesome"}
+          color={"#696969"}
+          onPress={() => props.navigation.toggleDrawer()}
+        />
+      }
+      centerComponent={{
+        text: props.title,
+        style: styles.title
+      }}
+      backgroundColor={"#6fc0b8"}
+    />
+  );
+};
+
+export default MyHeader;
+
+const styles = StyleSheet.create({
+  title: {
+    color: "#fff",
+    fontSize: 25,
+    fontWeight: "bold"
+  }
+});
